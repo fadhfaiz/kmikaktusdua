8356c984c4a980c60b1fd9434ab8098fd59917e4
fadhfaiz <faadh.faaiz@gmail.com>
2019-04-23 18:41:29 +0700

    versi 4.0 update

--- application/views/adminpage/product_admin.php
+++ application/views/adminpage/product_admin.php
@@ -1,5 +1,73 @@
-<section>
-    <div class="container mt-5">
-        <div class="text-center">Tampilan Halaman Admin Produk</div>
-    </div>
+<section class="container mt-4">
+	<form action="">
+		<div class="row">
+			<div class="col-9"><input class="form-control" type="text" placeholder="Cari tanaman"></div>
+			<div class="col-3"><button class="btn btn-block btn-warning">Search</button></div>
+		</div>
+	</form>
+</section>
+
+<section class="container">
+	<div class="card-deck my-4">
+		<div class="card">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"
+				style="color:black; text-decoration: none;"><img src="<?php echo base_url()?>assets/img/edit1.jpg"
+					class="card-img-top" alt="...">
+				<div class="card-body">
+					<h6 class="card-title">Tanaman Kaktus</h6>
+				</div>
+			</a>
+			<div class="card-footer">
+				<small class="text-muted">Rp. 360,000</small>
+			</div>
+		</div>
+		<div class="card">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"
+				style="color:black; text-decoration: none;"><img src="<?php echo base_url()?>assets/img/edit2.jpg"
+					class="card-img-top" alt="...">
+				<div class="card-body">
+					<h6 class="card-title">Tanaman Kaktus</h6>
+				</div>
+			</a>
+			<div class="card-footer">
+				<small class="text-muted">Rp. 360,000</small>
+			</div>
+		</div>
+		<div class="card">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"
+				style="color:black; text-decoration: none;"><img src="<?php echo base_url()?>assets/img/edit1.jpg"
+					class="card-img-top" alt="...">
+				<div class="card-body">
+					<h6 class="card-title">Tanaman Kaktus</h6>
+				</div>
+			</a>
+			<div class="card-footer">
+				<small class="text-muted">Rp. 360,000</small>
+			</div>
+		</div>
+		<div class="card">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"
+				style="color:black; text-decoration: none;"><img src="<?php echo base_url()?>assets/img/edit2.jpg"
+					class="card-img-top" alt="...">
+				<div class="card-body">
+					<h6 class="card-title">Tanaman Kaktus</h6>
+				</div>
+			</a>
+			<div class="card-footer">
+				<small class="text-muted">Rp. 360,000</small>
+			</div>
+		</div>
+		<div class="card">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"
+				style="color:black; text-decoration: none;"><img src="<?php echo base_url()?>assets/img/edit1.jpg"
+					class="card-img-top" alt="...">
+				<div class="card-body">
+					<h6 class="card-title">Tanaman Kaktus</h6>
+				</div>
+			</a>
+			<div class="card-footer">
+				<small class="text-muted">Rp. 360,000</small>
+			</div>
+		</div>
+	</div>
 </section>
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -14,6 +14,112 @@
 </header>
 
 <section class="container">
+	<div class="container my-5">
+		<div class="card-deck">
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/product_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Kaktus</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/article_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Aglonema</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.
+						</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/verification_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Aloevera</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="container">
+	<div class="container my-5">
+		<div class="card-deck">
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/product_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Kaktus</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/article_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Aglonema</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.
+						</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/verification_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Aloevera</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- <section class="container">
 	<div class="row my-5 text-white">
 		<div class="col-sm-4">
 			<div class="card" id="card-satu">
@@ -49,7 +155,7 @@
 			</div>
 		</div>
 	</div>
-</section>
+</section> -->
 
 <section class="container">
 	<div>
@@ -57,7 +163,8 @@
 	</div>
 	<div class="card-deck my-3">
 		<div class="card">
-			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img
+					src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h6 class="card-title">Sansiveera</h6>
 				</div>
@@ -67,7 +174,8 @@
 			</div>
 		</div>
 		<div class="card">
-			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img src="<?php echo base_url()?>assets/img/edit1.jpg" class="card-img-top" alt="...">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img
+					src="<?php echo base_url()?>assets/img/edit1.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h6 class="card-title">Sansiveera</h6>
 				</div>
@@ -77,7 +185,8 @@
 			</div>
 		</div>
 		<div class="card">
-			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img
+					src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h6 class="card-title">Sansiveera</h6>
 				</div>
@@ -87,7 +196,8 @@
 			</div>
 		</div>
 		<div class="card">
-			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img src="<?php echo base_url()?>assets/img/edit1.jpg" class="card-img-top" alt="...">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img
+					src="<?php echo base_url()?>assets/img/edit1.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h6 class="card-title">Sansiveera</h6>
 				</div>
@@ -97,7 +207,8 @@
 			</div>
 		</div>
 		<div class="card">
-			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
+			<a href="<?php echo base_url(); ?>marketplace/detail_product"><img
+					src="<?php echo base_url()?>assets/img/edit2.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h6 class="card-title">Sansiveera</h6>
 				</div>
@@ -113,44 +224,57 @@
 </section>
 
 <section class="container">
-	<div class="mt-5 mb-4">
-		<h4 class="text-center">Aksesoris Perlengkapan Tanaman</h4>
+	<div class="container my-5">
+		<div class="card-deck">
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/product_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Pot Tanaman</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/article_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Bibit Tanaman</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.
+						</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+
+			<div class="card bg-dark"><a href="<?php echo base_url(); ?>adminpage/verification_admin"
+					style="color:white; text-decoration: none;">
+					<div class="card-body">
+						<h5 class="card-title text-center">Batu Tanaman</h5>
+						<hr>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional
+							content. This content is a little bit longer.</p>
+					</div>
+				</a>
+				<!-- <div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div> -->
+			</div>
+		</div>
 	</div>
-	<div class="row mb-5 text-white">
-		<div class="col-sm-4">
-			<div class="card" id="card-satu">
-				<div class="card-body">
-					<h5 class="card-title">Pot Tanaman</h5>
-					<p class="card-text font-italic">With supporting text below as a natural lead-in to
-						additional content.</p>
-					<a href="#" class="btn btn-outline-secondary" disabled><i
-							class="fas fa-cart-arrow-down fa-lg mx-2"></i></a>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4">
-			<div class="card" id="card-satu">
-				<div class="card-body">
-					<h5 class="card-title">Batu</h5>
-					<p class="card-text font-italic">With supporting text below as a natural lead-in to
-						additional content.</p>
-					<a href="#" class="btn btn-outline-secondary" disabled><i
-							class="fas fa-cart-arrow-down fa-lg mx-2"></i></a>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4">
-			<div class="card" id="card-satu">
-				<div class="card-body">
-					<h5 class="card-title">Batu</h5>
-					<p class="card-text text-italic">With supporting text below as a natural lead-in to
-						additional content.</p>
-					<a href="#" class="btn btn-outline-secondary" disabled><i
-							class="fas fa-cart-arrow-down fa-lg mx-2"></i></a>
-				</div>
-			</div>
-		</div>
-	</div>
+
 	<a href="https://www.instagram.com/fadh.leather/" class="float-ig" target="_blank">
 		<i class="fab fa-instagram my-float"></i>
 	</a>
