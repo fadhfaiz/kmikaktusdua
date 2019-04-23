f8a912777c5e63ef821473882e984fc67b9f106e
fadhfaiz <faadh.faaiz@gmail.com>
2019-04-23 15:09:25 +0700

    versi 3.4 update

--- application/views/adminpage/loginpage.php
+++ application/views/adminpage/loginpage.php
@@ -7,6 +7,7 @@
 		<div class="col-lg-4 col-md-12 col-sm-12">
 			<hr>
 			<form action="">
+			
 				<div class="form-group">
 					<label for="exampleInputEmail1" style="color: teal;">ID Admin</label>
 					<input type="text" class="form-control font-italic" id="exampleInputEmail1" aria-describedby="emailHelp"
@@ -14,6 +15,7 @@
 					<small id="emailHelp" class="form-text text-muted">We'll never share your admin ID with anyone
 						else.</small>
 				</div>
+
 				<div class="form-group">
 					<label for="exampleInputPassword1" style="color: teal;">Password</label>
 					<input type="password" class="form-control font-italic" id="exampleInputPassword1" placeholder="enter your password">
