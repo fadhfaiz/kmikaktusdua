e33b99fd35c0e4a67cf6f69953a05cae64a8587a
IDestiyanti <intandesti25@gmail.com>
2019-04-23 14:14:10 +0700

    pembayaran

--- application/views/adminpage/detail_verification.php
+++ application/views/adminpage/detail_verification.php
@@ -1,17 +1,72 @@
 <br><br>
 <div class="container">
-<div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
+<div class="row">
+  <div class="col"><h2>Detail Pesanan</h2></div>
+  <div class="col"><h2>Kode Pesanan : 123456</h2></div>
+</div>
+<div class="card text-left">
+
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="row">
+    <div class="col-4"><h4 class="card-title">Status</h4></div>
+    <div class="col-1"><h4 class="card-title">:</h4></div>
+    <div class="col"><h4 class="card-title">Menunggu biaya ongkir dari penjual</h4></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Nama Produk</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Jumlah</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Harga Produk</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Ongkos Kirim</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+   
   </div>
   <div class="card-footer text-muted">
-    2 days ago
+  <div class="row">
+    <div class="col-4"><h5 class="card-text">Total Pembayaran</h5></div>
+    <div class="col-1"><h5 class="card-text">:</h5></div>
+    <div class="col"><h5 class="card-text"></h5></div>
+  </div>
+   
   </div>
 </div>
-
+</div>
+<br><br>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card text-white bg-secondary mb-3" ><a href="<?php echo base_url(); ?>article/artikel" style="color:white; text-decoration: none;">
+        <div class="card-body text-center">
+         <h4 class="card-title">Menunggu Ongkir</h4>
+       </div></a>
+     </div>
+    </div>
+    <div class="col">
+      <div class="card boder-secondary mb-3" >
+        <div class="card-body text-center">
+         <h4 class="card-title">Menunggu Pembayaran</h4>
+       </div>
+     </div>
+    </div>
+    <div class="col">
+      <div class="card boder-secondary mb-3" >
+        <div class="card-body text-center">
+         <h4 class="card-title">Pembayaran Terkonfirmasi</h4>
+       </div>
+     </div>
+    </div>
+    </div>
 </div>
--- application/views/pembelian/status_kirim.php
+++ application/views/pembelian/status_kirim.php
@@ -1,3 +1,72 @@
-<section>
+<br><br>
+<div class="container">
+<div class="row">
+  <div class="col"><h2>Detail Pesanan</h2></div>
+  <div class="col"><h2>Kode Pesanan : 123456</h2></div>
+</div>
+<div class="card text-left">
 
-</section>
+  <div class="card-body">
+  <div class="row">
+    <div class="col-4"><h4 class="card-title">Status</h4></div>
+    <div class="col-1"><h4 class="card-title">:</h4></div>
+    <div class="col"><h4 class="card-title">Menunggu biaya ongkir dari penjual</h4></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Nama Produk</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Jumlah</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Harga Produk</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+  <div class="row">
+    <div class="col-4"><h5 class="card-title">Ongkos Kirim</h5></div>
+    <div class="col-1"><h5 class="card-title">:</h5></div>
+    <div class="col"><h5 class="card-title"></h5></div>
+  </div>
+   
+  </div>
+  <div class="card-footer text-muted">
+  <div class="row">
+    <div class="col-4"><h5 class="card-text">Total Pembayaran</h5></div>
+    <div class="col-1"><h5 class="card-text">:</h5></div>
+    <div class="col"><h5 class="card-text"></h5></div>
+  </div>
+   
+  </div>
+</div>
+</div>
+<br><br>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card text-white bg-secondary mb-3" ><a href="<?php echo base_url(); ?>article/artikel" style="color:white; text-decoration: none;">
+        <div class="card-body text-center">
+         <h4 class="card-title">Menunggu Ongkir</h4>
+       </div></a>
+     </div>
+    </div>
+    <div class="col">
+      <div class="card boder-secondary mb-3" >
+        <div class="card-body text-center">
+         <h4 class="card-title">Menunggu Pembayaran</h4>
+       </div>
+     </div>
+    </div>
+    <div class="col">
+      <div class="card boder-secondary mb-3" >
+        <div class="card-body text-center">
+         <h4 class="card-title">Pembayaran Terkonfirmasi</h4>
+       </div>
+     </div>
+    </div>
+    </div>
+</div>
