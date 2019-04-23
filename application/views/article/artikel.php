22104a4cb0cffb8a388e86c0e207e20e9dd58dc5
IDestiyanti <intandesti25@gmail.com>
2019-04-23 17:13:27 +0700

    artikel

--- application/views/article/artikel.php
+++ application/views/article/artikel.php
@@ -1,12 +1,25 @@
 <br><br>
-<div class="container">
-    <center>
-<div class="col-lg-10 mt-4 mb-4">
-<div class="card bg-dark" >
-  <img src="<?php echo base_url()?>assets/img/kaktus01.jpg" class="img-fluid" alt="Responsive image">
-</div>
-</a>
-</div>
-</center>
-</div>
+  <div class="container">
+        <div class="row">
+           <h1>Lorem Ipsum Dolor Sit Amet</h1>
+        </div>
+        <div class="row">
+           <div class="col"><h4 style="text-align:right">DD/MM/YYYY</h4></div>
+        </div>
+        <div class="row">
+          <div class="col mt-4 mb-4">
+              <div class="card bg-dark" >
+                <img src="<?php echo base_url()?>assets/img/kaktus01.jpg" class="img-fluid" alt="Responsive image">
+              </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col mt-4 mb-4">
+          <div id="artikel">
+              <p>Alkisah, Dayang Sumbi beranjak dewasa hingga pada akhirnya dipersunting oleh seorang pria. Hasil dari kisah cinta mereka, lahirlah seorang pria bernama Sangkuriang. Ketika Sangkuriang beranjak dewasa, suami Dayang Sumbi tidak berumur panjang. </p>
+              <p>Sangkuriang semasa hidupnya memiliki kegemaran sama seperti sang prabu. Sehingga pada suatu ketika Sangkuriang bersama Tumang hendak mengadakan perburuan dengan maksud ingin membahagiakan sang ibu. Namun perburuan tidak berbuah hasil. Tumang pun jadi Tumbal. Tanpa pikir panjang, Tumang dibunuh yang kemudian memberikan hati kepada ibunya.</p>
+              <p>Mengetahui hati tersebut yang tiada lain adalah hati Tumang, si anjing kesayangannya, Dayang Sumbi murka. Dengan amarah, dia menghantamkan benda kepada Sangkuriang hingga membekas di kepalanya.</p>
+          </div>
+          </div>
+  </div>
 
