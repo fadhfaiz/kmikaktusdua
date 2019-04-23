cb2fa4696afd96c0240468727b2353fa930c6023
fadhfaiz <faadh.faaiz@gmail.com>
2019-04-23 15:04:46 +0700

    versi 3.2

--- application/views/adminpage/detail_verification.php
+++ application/views/adminpage/detail_verification.php
@@ -1,104 +1,175 @@
-<br><br>
-<div class="container">
-<div class="row">
-<div class="col">
-<div class="row">
-  <div class="col"><h2>Detail Pesanan</h2></div>
-</div>
-<div class="card text-left">
+<div class="container my-5">
 
-  <div class="card-body">
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Nama Produk</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Jumlah</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Berat</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Diameter x Tinggi</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>  
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Total Bayar</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-   
-  </div>
-  <div class="card-footer text-muted">
-    <div class="row">
-      <div class="col">
-          <form>
-            <div class="form-group">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan ongkos kirim">
-            </div>
-          </form>
-      </div>
-      <div class="col-0"><button type="button" class="btn btn-secondary">Submit</button></div>
-      <div class="col"><button type="button" class="btn btn-primary">Konfirmasi</button></div>
-    </div>
-  </div>
-</div>
-</div>
-<div class="col">
-<div class="row">
-  <div class="col"><h2>Kode Pesanan : 123456</h2></div>
-</div>
-<div class="card text-left">
+	<div class="row">
+		<div class="col-lg-6 col-md-12 col-sm-12">
 
-  <div class="card-body">
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Nama Produk</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Jumlah</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Berat</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Diameter x Tinggi</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>  
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Total Bayar</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-   
-  </div>
-  <div class="card-footer text-muted">
-    <div class="row">
-      <div class="col">
-          <form>
-            <div class="form-group">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan ongkos kirim">
-            </div>
-          </form>
-      </div>
-      <div class="col-0"><button type="button" class="btn btn-secondary">Submit</button></div>
-      <div class="col"><button type="button" class="btn btn-primary">Konfirmasi</button></div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
+			<div class="row my-2">
+				<div class="col">
+					<h2>Detail Pesanan</h2>
+				</div>
+			</div>
+
+			<div class="card text-left">
+				<div class="card-body">
+
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Nama Produk</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Jumlah</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Berat</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Diameter x Tinggi</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Total Bayar</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+				</div>
+				<div class="card-footer text-muted">
+					<div class="row">
+						<div class="col">
+							<form>
+								<div class="form-group">
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+										placeholder="Masukkan ongkos kirim">
+								</div>
+							</form>
+						</div>
+						<div class="col-0"><button type="button" class="btn btn-secondary">Submit</button></div>
+						<div class="col"><button type="button" class="btn btn-primary">Konfirmasi</button></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-6 col-md-12 col-sm-12">
+			<div class="row my-2">
+
+				<div class="col">
+					<h2>Kode Pesanan : 123456</h2>
+				</div>
+
+			</div>
+
+			<div class="card text-left">
+				<div class="card-body">
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Nama Produk</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Jumlah</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Berat</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Diameter x Tinggi</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<h5 class="card-title">Total Bayar</h5>
+						</div>
+						<div class="col-1">
+							<h5 class="card-title">:</h5>
+						</div>
+						<div class="col">
+							<h5 class="card-title"></h5>
+						</div>
+					</div>
+
+				</div>
+				<div class="card-footer text-muted">
+					<div class="row">
+						<div class="col">
+							<form>
+								<div class="form-group">
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+										placeholder="Masukkan ongkos kirim">
+								</div>
+							</form>
+						</div>
+						<div class="col-0"><button type="button" class="btn btn-secondary">Submit</button></div>
+						<div class="col"><button type="button" class="btn btn-primary">Konfirmasi</button></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </div>
--- application/views/pembelian/status_kirim.php
+++ application/views/pembelian/status_kirim.php
@@ -1,72 +1,132 @@
 <br><br>
 <div class="container">
-<div class="row">
-  <div class="col"><h2>Detail Pesanan</h2></div>
-  <div class="col"><h2>Kode Pesanan : 123456</h2></div>
-</div>
-<div class="card text-left">
 
-  <div class="card-body">
-  <div class="row">
-    <div class="col-4"><h4 class="card-title">Status</h4></div>
-    <div class="col-1"><h4 class="card-title">:</h4></div>
-    <div class="col"><h4 class="card-title">Menunggu biaya ongkir dari penjual</h4></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Nama Produk</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Jumlah</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Harga Produk</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-  <div class="row">
-    <div class="col-4"><h5 class="card-title">Ongkos Kirim</h5></div>
-    <div class="col-1"><h5 class="card-title">:</h5></div>
-    <div class="col"><h5 class="card-title"></h5></div>
-  </div>
-   
-  </div>
-  <div class="card-footer text-muted">
-  <div class="row">
-    <div class="col-4"><h5 class="card-text">Total Pembayaran</h5></div>
-    <div class="col-1"><h5 class="card-text">:</h5></div>
-    <div class="col"><h5 class="card-text"></h5></div>
-  </div>
-   
-  </div>
-</div>
+	<div class="row">
+		<div class="col">
+			<h3>Detail Pesanan</h3>
+		</div>
+	</div>
+
+	<div class="card text-left">
+		<div class="card-body text-muted">
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Kode Pesanan</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title">CSK64H</h5>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Status</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title">Menunggu biaya ongkir dari penjual</h5>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Nama Produk</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title"></h5>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Jumlah</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title"></h5>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Harga Produk</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title"></h5>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-title">Ongkos Kirim</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-title">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-title"></h5>
+				</div>
+			</div>
+		</div>
+
+		<div class="card-footer text-muted">
+			<div class="row">
+				<div class="col-4">
+					<h5 class="card-text">Total Pembayaran</h5>
+				</div>
+				<div class="col-1">
+					<h5 class="card-text">:</h5>
+				</div>
+				<div class="col-7">
+					<h5 class="card-text"></h5>
+				</div>
+			</div>
+
+		</div>
+	</div>
 </div>
 <br><br>
+
 <div class="container">
-  <div class="row">
-    <div class="col">
-      <div class="card text-white bg-secondary mb-3" ><a href="<?php echo base_url(); ?>article/artikel" style="color:white; text-decoration: none;">
-        <div class="card-body text-center">
-         <h4 class="card-title">Menunggu Ongkir</h4>
-       </div></a>
-     </div>
-    </div>
-    <div class="col">
-      <div class="card boder-secondary mb-3" >
-        <div class="card-body text-center">
-         <h4 class="card-title">Menunggu Pembayaran</h4>
-       </div>
-     </div>
-    </div>
-    <div class="col">
-      <div class="card boder-secondary mb-3" >
-        <div class="card-body text-center">
-         <h4 class="card-title">Pembayaran Terkonfirmasi</h4>
-       </div>
-     </div>
-    </div>
-    </div>
+	<div class="row">
+
+		<div class="col">
+			<div class="card text-white bg-secondary mb-3">
+					<div class="card-body text-center">
+						<h5 class="card-title">Menunggu Ongkir</h5>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div class="col">
+			<div class="card boder-secondary mb-3">
+				<div class="card-body text-center">
+					<h5 class="card-title">Menunggu Pembayaran</h5>
+				</div>
+			</div>
+		</div>
+
+		<div class="col">
+			<div class="card boder-secondary mb-3">
+				<div class="card-body text-center">
+					<h5 class="card-title">Pembayaran Terkonfirmasi</h5>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
