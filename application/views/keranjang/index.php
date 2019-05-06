a98132d51216516bf9926935c64cca8bb93b38db
fadhfaiz <faadh.faaiz@gmail.com>
2019-05-06 12:29:02 +0700

    maaf saya terlambat

--- application/views/keranjang/index.php
+++ application/views/keranjang/index.php
@@ -1,3 +1,39 @@
 <section class="container">
+	<table class="table table-bordered mt-5">
+		<thead class="text-center">
+			<tr>
+				<th scope="col">Foto Barang Barang</th>
+				<th scope="col">Nama Barang</th>
+				<th scope="col">Harga @</th>
+				<th scope="col">Jumlah Pembelian</th>
+                <th scope="col">Harga Total</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">Kaktus.jpg</th>
+				<td>Kaktus Durian Sumatera</td>
+				<td class="text-center">Rp 35.000</td>
+				<td class="text-center">3</td>
+                <td class="text-center">Rp 105.000</td>
+			</tr>
+			<tr>
+				<th scope="row">Aglonema.png</th>
+				<td>Aglonema Bantul</td>
+				<td class="text-center">Rp 55.000</td>
+				<td class="text-center">2</td>
+                <td class="text-center">Rp 110.000</td>
+			</tr>
+			<tr>
+				<td scope="row" colspan="4" class="text-center font-weight-bold">Jumlah Total Pembayaran</td>
+                <td class="text-center" style="color: red;">Rp 215.000</td>
+			</tr>
+		</tbody>
+	</table>
 
+    <div class="row mt-3">
+        <div class="col-6"></div>
+        <div class="col-3"><input type="button" value="Kembali" class="btn btn-outline-info btn-block"></div>
+        <div class="col-3"><input type="button" value="Checkout" class="btn btn-success btn-block"></div>
+    </div>
 </section>
