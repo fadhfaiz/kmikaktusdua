e06adee570a323aad55a87cbe39c9b3b5f63afcb
Oko Carono <okocarono@gmail.com>
2019-05-14 12:13:25 +0700

    update database

---
+++ database/kmikaktus.sql
@@ -0,0 +1,273 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2019 at 07:06 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kmikaktus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE `artikel` (
+  `kode_artikel` int(11) NOT NULL,
+  `judul` varchar(60) NOT NULL,
+  `isi` text NOT NULL,
+  `tanggal` int(11) NOT NULL,
+  `gambar` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artikel`
+--
+
+INSERT INTO `artikel` (`kode_artikel`, `judul`, `isi`, `tanggal`, `gambar`) VALUES
+(9, 'kaktusku', '<p>sadfasdf</p>', 1557640669, 'gambar2.jpg'),
+(11, 'Aglaonema Adelia', '<p>ytyterere</p>', 1557645437, 'gambar12.jpg'),
+(13, 'kaktusku', '<p>alhamdulillah</p>', 1557646955, 'gambar8.jpg'),
+(15, 'laktea', '<p>huuuu</p>', 1557650583, 'gambar6.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` char(5) NOT NULL,
+  `nama_pembeli` varchar(40) DEFAULT NULL,
+  `no_telp` char(13) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `kecamatan` varchar(35) DEFAULT NULL,
+  `kabupaten` varchar(35) DEFAULT NULL,
+  `provinsi` varchar(35) DEFAULT NULL,
+  `kodepos` char(5) NOT NULL,
+  `alamat_lengkap` varchar(50) DEFAULT NULL,
+  `catatan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id_pembeli`, `nama_pembeli`, `no_telp`, `email`, `kecamatan`, `kabupaten`, `provinsi`, `kodepos`, `alamat_lengkap`, `catatan`) VALUES
+('PB04', '', '', '', '', '', '', '', '', ''),
+('PB4', 'Ari', '08761234666', 'arinurcahya@gmail.com', 'umbulharjo', 'bantul', 'Yogyakarta', '55164', 'hdgskjdaslkh.kjdg', 'coba yaa'),
+('PM001', 'Wahyu Shofian', '085200554942', 'wahyushofian@gmail.com', 'Nitikan', 'Bantul', 'Yogyakarta', '55571', 'Jl. Nitikan, Bantul, DIY', 'Jangan kirim barang second ya gan'),
+('PM002', 'Faadihilah Ahnaf Faiz', '08577432571', 'fadhfaiz@gmail.com', 'Sewon', 'Bantul', 'Yogyakarta', '53718', 'Widoro, Bangunharjo, Mbantul', 'Jangan kirim dompet kulit ya gan'),
+('PM003', 'Intan Destiyanti', '082132454333', 'idestiyanti@gmail.com', 'Umbulharjo', 'Bantul', 'Yogyakarta', '53218', 'Jl. Dr. Soepomo, Umbulharjo', 'Packing mantap gan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjual`
+--
+
+CREATE TABLE `penjual` (
+  `id_penjual` char(1) NOT NULL,
+  `nama_penjual` varchar(40) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telp` char(13) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjual`
+--
+
+INSERT INTO `penjual` (`id_penjual`, `nama_penjual`, `email`, `no_telp`, `username`, `password`) VALUES
+('1', 'Dewi Soyusiawaty', 'my_soyus@yahoo.com', '085200554942', 'kaktuskmi', 'aglonema');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `kode_produk` char(5) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `harga_produk` int(11) NOT NULL,
+  `stok_produk` int(3) NOT NULL,
+  `diameter` int(3) NOT NULL,
+  `tinggi` int(3) NOT NULL,
+  `gambar` varchar(20) NOT NULL,
+  `jenis_produk` varchar(30) NOT NULL,
+  `catatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbcounter`
+--
+
+CREATE TABLE `tbcounter` (
+  `ip` varchar(2050) NOT NULL,
+  `date` date NOT NULL,
+  `hits` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbcounter`
+--
+
+INSERT INTO `tbcounter` (`ip`, `date`, `hits`) VALUES
+('10.88.25.1', '2016-01-16', 11),
+('10.88.25.2', '2016-01-16', 3),
+('10.88.25.3', '2016-01-17', 4),
+('10.88.25.4', '2016-01-16', 11),
+('10.88.25.5', '2016-02-16', 3),
+('10.88.25.6', '2016-02-17', 4),
+('10.88.25.10', '2016-02-23', 2),
+('10.88.25.15', '2016-02-23', 1),
+('10.88.25.13', '2016-03-16', 11),
+('10.88.25.1', '2016-03-16', 3),
+('10.88.25.5', '2016-03-17', 4),
+('10.88.25.8', '2016-03-16', 11),
+('10.88.25.10', '2016-03-16', 3),
+('10.88.25.19', '2016-03-17', 4),
+('10.88.25.27', '2016-04-23', 2),
+('10.88.25.50', '2016-04-23', 1),
+('10.88.25.6', '2016-04-16', 3),
+('10.88.25.9', '2016-05-17', 4),
+('10.88.25.11', '2016-05-23', 2),
+('10.88.25.12', '2016-05-23', 1),
+('10.88.25.16', '2016-05-16', 11),
+('10.88.25.20', '2016-05-16', 3),
+('10.88.25.32', '2016-06-17', 4),
+('10.88.25.1', '2016-06-16', 11),
+('10.88.25.35', '2016-06-16', 3),
+('10.88.25.36', '2016-07-17', 4),
+('10.88.25.26', '2016-07-23', 2),
+('10.88.25.6', '2016-07-16', 3),
+('10.88.25.9', '2016-08-17', 4),
+('10.88.25.11', '2016-08-23', 2),
+('10.88.25.12', '2016-08-23', 1),
+('10.88.25.16', '2016-08-16', 11),
+('10.88.25.20', '2016-08-06', 3),
+('10.88.25.32', '2016-09-17', 4),
+('10.88.25.1', '2016-09-16', 11),
+('10.88.25.35', '2016-09-16', 3),
+('10.88.25.36', '2016-09-17', 4),
+('10.88.25.26', '2016-09-23', 2),
+('10.88.25.16', '2016-10-16', 11),
+('10.88.25.20', '2016-10-16', 3),
+('10.88.25.32', '2016-10-17', 4),
+('10.88.25.1', '2016-10-16', 11),
+('10.88.25.35', '2016-11-16', 3),
+('10.88.25.36', '2016-11-17', 4),
+('10.88.25.26', '2016-11-23', 2),
+('10.88.25.15', '2016-11-23', 1),
+('10.88.25.13', '2016-12-16', 11),
+('10.88.25.1', '2016-12-16', 3),
+('10.88.25.5', '2016-12-17', 4),
+('10.88.25.8', '2016-12-16', 11),
+('10.88.25.10', '2016-12-16', 3),
+('10.88.25.19', '2016-12-17', 4),
+('10.88.25.27', '2016-12-23', 2),
+('10.88.25.50', '2016-12-23', 1),
+('10.88.25.6', '2016-12-16', 3),
+('10.88.25.9', '2016-12-17', 4),
+('10.88.25.11', '2016-12-23', 2),
+('10.88.25.12', '2016-12-23', 1),
+('10.88.25.20', '2016-12-25', 1),
+('::1', '2019-05-08', 5),
+('::1', '2019-05-11', 3),
+('::1', '2019-05-12', 6),
+('::1', '2019-05-14', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `kode_pesanan` char(5) NOT NULL,
+  `id_pembeli` char(5) DEFAULT NULL,
+  `kode_produk` char(5) DEFAULT NULL,
+  `total_harga` int(10) DEFAULT NULL,
+  `ongkir` int(10) DEFAULT NULL,
+  `status` varchar(25) DEFAULT NULL,
+  `jumlah_produk` int(3) DEFAULT NULL,
+  `tanggal_beli` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`kode_artikel`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id_pembeli`);
+
+--
+-- Indexes for table `penjual`
+--
+ALTER TABLE `penjual`
+  ADD PRIMARY KEY (`id_penjual`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`kode_produk`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`kode_pesanan`),
+  ADD KEY `id_pembeli` (`id_pembeli`,`kode_produk`),
+  ADD KEY `kode_produk` (`kode_produk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `kode_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`kode_produk`) REFERENCES `produk` (`kode_produk`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
