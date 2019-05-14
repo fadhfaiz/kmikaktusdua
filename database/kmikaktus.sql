6fd30ac2dc382576a86a26cd89c26d4d0311b01a
Oko Carono <okocarono@gmail.com>
2019-05-14 12:30:06 +0700

    Update kmikaktus.sql

--- database/kmikaktus.sql
+++ database/kmikaktus.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2019 at 07:06 AM
+-- Generation Time: May 14, 2019 at 07:28 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.21
 
